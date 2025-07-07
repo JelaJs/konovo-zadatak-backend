31b96513a868fb2f3704c70b05c2f91a44781562
JelaJs <nikola4434@gmail.com>
2025-07-07 21:02:06 +0200

    * Listed all products

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ProductsController extends Controller
 {
-    public function index(): JsonResponse
+    public function index(Request $request): JsonResponse
     {
-        return response()->json('test');
+        $token = $request->bearerToken();
+        $ApiResponse = Http::withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->get('https://zadatak.konovo.rs/products');
+
+        return response()->json($ApiResponse->json());
     }
 }
