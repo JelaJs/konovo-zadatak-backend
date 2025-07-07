4748f47110dd0b283692aa8295c83d3c578769f2
JelaJs <nikola4434@gmail.com>
2025-07-07 20:47:56 +0200

    * Created API endpoint, ProductsController, controller method for listing all products from external api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProductsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test', function (Request $request) {
-    return response()->json(['message' => 'test']);
+Route::prefix('v1')->group(function () {
+    Route::controller(ProductsController::class)->prefix('/products')->group(function () {
+        Route::get('/', 'index');
+    });
 });
